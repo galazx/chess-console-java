a50da42e80669cf32d73129725701388687009d8
galazx <thiago.maricondi@gmail.com>
2024-03-14 11:29:37 -0300

    contador de movimento

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,6 +6,7 @@ import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
 	private Color color;
+	private int moveCount;
 	
 	public ChessPiece(Board board, Color color) {
 		super(board);
@@ -23,5 +24,14 @@ public abstract class ChessPiece extends Piece {
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p != null && p.getColor() != this.color;
+	}
+	public int getMoveCount() {
+		return moveCount;
+	}
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+	public void decreaseMoveCount() {
+		moveCount--;
 	}
 }
