d3e9a7eb98797ca5f4fc0f2e2dac42ab294c31af
galazx <thiago.maricondi@gmail.com>
2024-03-19 10:28:30 -0300

    promocao

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,12 @@ public class Program {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Entre com a peça para promoção (B/N/Q/R): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}catch(ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
